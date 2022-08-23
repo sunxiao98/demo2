93b02c7fab8b69ae63ec0cdd984b18cf5033ded6
孙啸 <1877147116@qq.com>
2022-08-23 11:08:13 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/PmsBrandController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controller;
+
+import com.example.demo.common.api.CommonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/**
+ * 品牌管理Controller
+ * Created by macro on 2019/4/19.
+ */
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
+@Controller
+@RequestMapping("/brand")
+public class PmsBrandController {
+
+}
---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+
+}
